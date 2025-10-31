93580e5bd773279b981502d784ef02402ac5b8d7
danig <daniguti05ps4@gmail.com>
2025-10-31 19:23:00 +0000

    Refactoring v6->v7: Move getAmount/getFrequentRentalPoints to Movie and rename; v7->v8: Extract Price class and move pricing logic

--- src/F3T2/Main.java
+++ src/F3T2/Main.java
@@ -13,9 +13,9 @@ public abstract class Main
 	public static void main(String[] args)
 	{
 		Customer who = new Customer("Barack Obama");
-		Movie m1 = new Movie("Life of Amalia", Movie.Code.REGULAR);
-		Movie m2 = new Movie("Peter Pan", Movie.Code.CHILDRENS);
-		Movie m3 = new Movie("Donna del Lago", Movie.Code.NEW_RELEASE);
+		Movie m1 = new Movie("Life of Amalia", new Price(Price.Code.REGULAR));
+		Movie m2 = new Movie("Peter Pan", new Price(Price.Code.CHILDRENS));
+		Movie m3 = new Movie("Donna del Lago", new Price(Price.Code.NEW_RELEASE));
 
 		who.addRental(new Rental(m1, 1));
 		who.addRental(new Rental(m2, 2));
--- src/F3T2/Rental.java
+++ src/F3T2/Rental.java
@@ -2,7 +2,7 @@ package F3T2;
 
 public class Rental
 {
-	private Movie _movie;
+	Movie _movie;
 	private int		_daysRented;
 
 	public Rental(Movie movie, int daysRented)
@@ -11,35 +11,7 @@ public class Rental
 		_daysRented = daysRented;
 	}
 
-    public int getFrequentRentalPoints()
-    {
-        return (getMovie().getPriceCode() == Movie.Code.NEW_RELEASE) && getDaysRented() > 1 ? 2 : 1;
-    }
-
-    public double getAmount() {
-        double thisAmount = 0;
-
-        // determine amounts for each line
-        switch (this.getMovie().getPriceCode())
-        {
-            case REGULAR:
-                thisAmount += 2;
-                if (this.getDaysRented() > 2)
-                    thisAmount += (this.getDaysRented() - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                thisAmount += this.getDaysRented() * 3;
-                break;
-            case CHILDRENS:
-                thisAmount += 1.5;
-                if (this.getDaysRented() > 3)
-                    thisAmount += (this.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
-    }
-
-	public int getDaysRented()
+    public int getDaysRented()
 	{
 		return _daysRented;
 	}
